1b797404ee92e4870632ef692ab33d432f4c1f7d
Hiroomi Kimura <643.cheesecake@gmail.com>
2017-04-09 14:36:12 +0000

    understant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource) 
     recommendations_path
   end
-  
-
 
   protected
 
--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,5 @@
 class RankingsController < ApplicationController
   def index
-    # binding.pry
     @books = Favorite.top10
     @books.each do |book|
       book.rakuten_data = RakutenWebService::Books::Total.search(isbnjan: book.isbn).first
